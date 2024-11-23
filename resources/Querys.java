a904fd520fb464d14152bc72576394123eb67f2b
Essa <essaalghinm+GitHub@gmail.com>
2024-11-23 16:09:13 +0300

    Update Querys.java

--- resources/Querys.java
+++ resources/Querys.java
@@ -1,203 +1,154 @@
 
 public class Querys {
 
-	public enum structure {
-		index, invertedIndex, invertedIndexBST, hashMap
-	}
+    public enum structure {
+        index, invertedIndex, invertedIndexBST, hashMap
+    }
 
-	private Index index;
-	private InvertedIndex invertedIndex;
-	private InvertedIndexBST invertedIndexBST;
-	private hashMap hashMap;
+    private Index index;
+    private InvertedIndex invertedIndex;
+    private InvertedIndexBST invertedIndexBST;
+    private hashMap hashMap;
 
-	public Querys(Index index, InvertedIndex invertedIndex, InvertedIndexBST invertedIndexBST, hashMap hashMap) {
-		this.index = index;
-		this.invertedIndex = invertedIndex;
-		this.invertedIndexBST = invertedIndexBST;
-		this.hashMap = hashMap;
-	}
+    public Querys(Index index, InvertedIndex invertedIndex, InvertedIndexBST invertedIndexBST, hashMap hashMap) {
+        this.index = index;
+        this.invertedIndex = invertedIndex;
+        this.invertedIndexBST = invertedIndexBST;
+        this.hashMap = hashMap;
+    }
 
-	private String and(String a, String b, structure s) {
+    private String searchByStructure(String term, structure s) {
+        switch (s) {
+            case index:
+                return index.search(term.trim());
+            case invertedIndex:
+                return invertedIndex.search(term.trim());
+            case invertedIndexBST:
+                return invertedIndexBST.search(term.trim());
+            case hashMap:
+                return hashMap.search(term.trim());
+            default:
+                return "Not found";
+        }
+    }
 
-		String strA = "";
-		String strB = "";
+    private String and(String a, String b, structure s) {
+        String strA = searchByStructure(a, s);
+        String strB = searchByStructure(b, s);
 
-		switch (s) {
-		case index:
-			strA = index.search(a.trim());
-			strB = index.search(b.trim());
-			break;
-		case invertedIndex:
-			strA = invertedIndex.search(a.trim());
-			strB = invertedIndex.search(b.trim());
+        if (strA.equalsIgnoreCase("Not found") || strB.equalsIgnoreCase("Not found"))
+            return "Not found";
 
-			break;
-		case invertedIndexBST:
-			strA = invertedIndexBST.search(a.trim());
-			strB = invertedIndexBST.search(b.trim());
+        StringBuilder temp = new StringBuilder();
 
-			break;
-		case hashMap:
-			strA = hashMap.search(a.trim());
-			strB = hashMap.search(b.trim());
+        String[] aa = strA.split("\\s+");
+        String[] bb = strB.split("\\s+");
 
-			break;
-		default:
-			strA = "Not found";
-			strB = "Not found";
+        if (!(aa[0].charAt(0) >= 48 && aa[0].charAt(0) <= 57))
+            return "one of the words are not found";
 
-		}
-		if (strA.equalsIgnoreCase("Not found") || strB.equalsIgnoreCase("Not found"))
-			return "Not found";
+        if (!(bb[0].charAt(0) >= 48 && bb[0].charAt(0) <= 57))
+            return "one of the words are not found";
 
-		StringBuilder temp = new StringBuilder();
+        int x = 0, y = 0;
 
-		String[] aa = strA.split("\\s+");
-		String[] bb = strB.split("\\s+");
+        while (true) {
+            if (Integer.parseInt(aa[x]) > Integer.parseInt(bb[y])) {
+                if (y != bb.length)
+                    y++;
+            } else if (Integer.parseInt(aa[x]) < Integer.parseInt(bb[y])) {
+                if (x != aa.length)
+                    x++;
+            } else {
+                temp.append(aa[x] + " ");
+                if (x != aa.length)
+                    x++;
+                if (y != bb.length)
+                    y++;
+            }
 
-		if (!(aa[0].charAt(0) >= 48 && aa[0].charAt(0) <= 57))
-			return "one of the words are not found";
+            if (x == aa.length || y == bb.length)
+                break;
+        }
+        
+        if (temp.toString().length() == 0) {
+            return "no intersections";
+        }
+        return temp.toString();
+    }
 
-		if (!(bb[0].charAt(0) >= 48 && bb[0].charAt(0) <= 57))
-			return "one of the words are not found";
+    private String or(String a, String b) {
+        StringBuilder temp = new StringBuilder();
+        String[] aa = a.trim().split("\\s+");
+        String[] bb = b.trim().split("\\s+");
 
-		int x = 0, y = 0;
+        if (!(aa[0].charAt(0) >= 48 && aa[0].charAt(0) <= 57))
+            return b;
 
-		while (true) {
-			if (Integer.parseInt(aa[x]) > Integer.parseInt(bb[y])) {
-				if (y != bb.length)
-					y++;
+        if (!(bb[0].charAt(0) >= 48 && bb[0].charAt(0) <= 57))
+            return a;
 
-			} else if (Integer.parseInt(aa[x]) < Integer.parseInt(bb[y])) {
-				if (x != aa.length)
-					x++;
+        int x = 0, y = 0;
 
-			} else {
-				temp.append(aa[x] + " ");
-				if (x != aa.length)
-					x++;
-				if (y != bb.length)
-					y++;
-			}
+        while (true) {
+            if (Integer.parseInt(aa[x]) > Integer.parseInt(bb[y])) {
+                temp.append(bb[y] + " ");
+                if (y != bb.length)
+                    y++;
+            } else if (Integer.parseInt(aa[x]) < Integer.parseInt(bb[y])) {
+                temp.append(aa[x] + " ");
+                if (x != aa.length)
+                    x++;
+            } else {
+                temp.append(aa[x] + " ");
+                if (x != aa.length)
+                    x++;
+                if (y != bb.length)
+                    y++;
+            }
+            
+            if (x == aa.length || y == bb.length)
+                break;
+        }
 
-			if (x == aa.length || y == bb.length)
-				break;
-		}
-		if (temp.toString().length() == 0) {
-			return "no intersections";
-		}
-		return temp.toString();
+        if (x < aa.length) {
+            for (int i = x; i < aa.length; i++)
+                temp.append(aa[i] + " ");
+        }
+        if (y < bb.length) {
+            for (int i = y; i < bb.length; i++)
+                temp.append(bb[i] + " ");
+        }
+        return temp.toString();
+    }
 
-	}
-
-	private String or(String a, String b) {
-
-		StringBuilder temp = new StringBuilder();
-		String[] aa = a.trim().split("\\s+");
-		String[] bb = b.trim().split("\\s+");
-
-		if (!(aa[0].charAt(0) >= 48 && aa[0].charAt(0) <= 57))
-			return b;
-
-		if (!(bb[0].charAt(0) >= 48 && bb[0].charAt(0) <= 57))
-			return a;
-
-		int x = 0, y = 0;
-
-		while (true) {
-
-			if (Integer.parseInt(aa[x]) > Integer.parseInt(bb[y])) {
-				temp.append(bb[y] + " ");
-
-				if (y != bb.length)
-					y++;
-
-			} else if (Integer.parseInt(aa[x]) < Integer.parseInt(bb[y])) {
-				temp.append(aa[x] + " ");
-				if (x != aa.length)
-					x++;
-
-			} else {
-				temp.append(aa[x] + " ");
-				if (x != aa.length)
-					x++;
-				if (y != bb.length)
-					y++;
-			}
-			if (x == aa.length || y == bb.length)
-
-				break;
-
-		}
-
-		if (x < aa.length) {
-			for (int i = x; i < aa.length; i++)
-				temp.append(aa[i] + " ");
-		}
-		if (y < bb.length) {
-			for (int i = y; i < bb.length; i++)
-				temp.append(bb[i] + " ");
-		}
-		return temp.toString();
-	}
-
-	public String Query(String k, structure s) {
-
-		String[] splitOr = k.split("\\b" + "or" + "\\b");
-		StringBuilder and = new StringBuilder();
-		String orAll = "";
-		for (int i = 0; i < splitOr.length; i++) {
-
-			String[] splitAnd = splitOr[i].split("\\b" + "and" + "\\b");
-			if (1 < splitAnd.length) {
-				String temp = "";
-				for (int j = 0; j < splitAnd.length; j++) {
-
-					if (j == 0)
-						temp = splitAnd[j];
-					else
-						temp = and(temp, splitAnd[j], s);
-
-				}
-				splitOr[i] = temp.trim();
-
-			}
-
-			else {
-				String str = "";
-
-				switch (s) {
-				case index:
-					str = index.search(splitAnd[0].trim()).trim();
-					break;
-				case invertedIndex:
-					str = invertedIndex.search(splitAnd[0].trim()).trim();
-					break;
-				case invertedIndexBST:
-					str = invertedIndexBST.search(splitAnd[0].trim()).trim();
-					break;
-				case hashMap:
-					str = hashMap.search(splitAnd[0].trim()).trim();
-					break;
-				default:
-					str = "Not found";
-
-				}
-
-				splitOr[i] = str;
-
-			}
-
-		}
-		for (int i = 0; i < splitOr.length; i++) {
-			if (i == 0)
-				orAll = splitOr[i];
-
-			else
-				orAll = or(orAll, splitOr[i]);
-
-		}
-		return orAll;
-	}
-
+    public String Query(String k, structure s) {
+        String[] splitOr = k.split("\\b" + "or" + "\\b");
+        String orAll = "";
+        
+        for (int i = 0; i < splitOr.length; i++) {
+            String[] splitAnd = splitOr[i].split("\\b" + "and" + "\\b");
+            
+            if (1 < splitAnd.length) {
+                String temp = "";
+                for (int j = 0; j < splitAnd.length; j++) {
+                    if (j == 0)
+                        temp = splitAnd[j];
+                    else
+                        temp = and(temp, splitAnd[j], s);
+                }
+                splitOr[i] = temp.trim();
+            } else {
+                splitOr[i] = searchByStructure(splitAnd[0], s);
+            }
+        }
+        
+        for (int i = 0; i < splitOr.length; i++) {
+            if (i == 0)
+                orAll = splitOr[i];
+            else
+                orAll = or(orAll, splitOr[i]);
+        }
+        return orAll;
+    }
 }
